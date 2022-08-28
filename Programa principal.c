c6ace07370eb4f1c05b3498b6870400b368edab4
GrandeDDD <106983267+GrandeDDD@users.noreply.github.com>
2022-08-28 15:19:50 -0300

    Update Programa principal.c

--- Programa principal.c
+++ Programa principal.c
@@ -1,6 +1,51 @@
 #include <stdio.h>
 
+//|=================================================================================|
+//|		Universidade Federal do Rio Grande do Norte (UFRN)							|
+//|		Centro de Ensino Superior do Serido											|
+//|		Departamento de Computacao e Tecnologia										|
+//|		Disciplina DCT1106 -> Programacao											|
+//|		Projeto Sistema de Planejamento de Dieta									|
+//|		Developed by "BrenoPorfirio" and "GrandeDDD" (github) -> August, 2022		|
+//|=================================================================================|
+//|		Semana 1																	|
+//|=================================================================================|
+
+//MODULOS DAS FUNCOES
+void modulo_menu(void);
+
+//INICIO DO PROGRAMA
 int main(void){
-    printf("Programa de Sistemas de Dietas");
+    modulo_menu();
 return 0;
+}
+
+//DEFININDO MODULOS
+//MODULO DE MENU:
+void modulo_menu(void){
+	printf("|=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=|");
+	printf("\n| Projeto: SIG-DietPlan                       |");
+	printf("\n|                                             |");
+	printf("\n| Descricao: Um sistema capaz auxiliar na     |");
+	printf("\n| escolha de um cardapio dependendo das       |");
+	printf("\n| restricoes impostas por um nutricionista.   |");
+	printf("\n|                                             |");
+	printf("\n| Programadores: Breno Costa (20220042506)    |");
+	printf("\n|                Diego Alves (20220043174)    |");
+	printf("\n|                                             |");
+	printf("\n| Professor: Flavius da Luz e Gorgonio        |");
+	printf("\n| (1687186)                                   |");
+	printf("\n|                                             |");
+	printf("\n| Programa produzido com intuito academico.   |");
+	printf("\n|                                             |");
+	printf("\n| Produzido sem fins comerciais em mente.     |");
+	printf("\n|                                             |");
+	printf("\n|                                             |");
+	printf("\n|                                             |");
+	printf("\n|                                             |");
+	printf("\n|                                             |");
+	printf("\n|                                             |");
+	printf("\n|                                             |");
+	printf("\n|                                             |");
+	printf("\n|=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=|");
 }
